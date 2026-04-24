4ab8e278d0eb8ec776836c9e812554539fa34196
louismukam-afk <louismukam@gmail.com>
2026-04-24 10:27:14 +0200

    Create migration file to add id_transfert_caisse column and foreign key constraint to decaissements table

---
+++ database/migrations/2026_04_24_000001_add_id_transfert_caisse_to_decaissements_table.php
@@ -0,0 +1,33 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIdTransfertCaisseToDecaissementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('decaissements', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_transfert_caisse')->after('some_column_name'); // change 'some_column_name' to actual column after which this should be added
+            $table->foreign('id_transfert_caisse')->references('id')->on('transfert_caisse')->onDelete('cascade');
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('decaissements', function (Blueprint $table) {
+            $table->dropForeign(['id_transfert_caisse']);
+            $table->dropColumn('id_transfert_caisse');
+        });
+    }
+}
