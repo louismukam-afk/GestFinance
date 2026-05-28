0a42f8939ad5dd403faf31a8726d076de441b64a
root <root@vmi1569407.contaboserver.net>
2026-05-28 18:32:25 +0200

    mise a jour email

--- resources/views/decaissements/etat_bons_financement.blade.php
+++ resources/views/decaissements/etat_bons_financement.blade.php
@@ -70,7 +70,7 @@
                             <a href="{{ route('etat_bons.show', $bon->id) }}" class="btn btn-info btn-sm">
                                 Details
                             </a>
-                               <a href="{{ route('decaissements.create', $bon->id) }}"
+                        <a href="{{ route('decaissements.create', $bon->id) }}"
                                class="btn btn-primary btn-sm">
                                 💸 Financer
                             </a>
